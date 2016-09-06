54123a44a274bdada1e13b82dd01b269fc09c912
masterUNG <phrombutr@gmail.com>
2016-09-06 11:10:48 +0700

    Start OK

--- app/src/main/java/com/mark1/novem/appebook/MainActivity.java
+++ app/src/main/java/com/mark1/novem/appebook/MainActivity.java
@@ -1,9 +1,7 @@
 package com.mark1.novem.appebook;
 
-import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String fontPath = "fonts/Cloud-Light.otf";
-        TextView txtCloud = (TextView)findViewById(R.id.dsa);
-        Typeface tf = Typeface.createFromAsset(getAssets(),fontPath);
-        txtCloud.setTypeface(tf);
-    }
-}
+
+    }   // Main Method
+
+}   // Main Class
